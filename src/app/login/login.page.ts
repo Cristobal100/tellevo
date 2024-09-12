f5b63e54171442291229b7999b5dcc17d2909a10
Cristobal100 <129971310+Cristobal100@users.noreply.github.com>
2024-09-11 23:53:25 -0300

    Add files via upload
    avance 11-09

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup,FormControl,Validators,FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -10,25 +10,37 @@ import { FormGroup,FormControl,Validators,FormBuilder } from '@angular/forms';
 export class LoginPage implements OnInit {
 
   formularioLogin: FormGroup;
+  passwordType: string = 'password';  // Variable para controlar el tipo de input
+  passwordIcon: string = 'eye-off';   // Variable para controlar el icono
 
   constructor(public fb: FormBuilder) { 
-    
     this.formularioLogin = this.fb.group({
-    'usuario': new FormControl("",Validators.required),
-    'contraseña': new FormControl("",Validators.required)
-  })
+      'usuario': new FormControl("", Validators.required),
+      'contraseña': new FormControl("", Validators.required)  // El formControlName aquí es "contraseña"
+    });
   }
 
-  ngOnInit() {
-  }
-  Ingresar(){
-    if(this.formularioLogin.valid){
-      const{usuario,password}=this.formularioLogin.value;
+  ngOnInit() { }
 
-      console.log('Usuario:',usuario);
-      console.log('password:',password);
-    }else{
-      console.log('datos invalidos');
+  Ingresar() {
+    if (this.formularioLogin.valid) {
+      const { usuario, contraseña } = this.formularioLogin.value;
+
+      console.log('Usuario:', usuario);
+      console.log('Contraseña:', contraseña);
+    } else {
+      console.log('Datos inválidos');
+    }
+  }
+
+  // Nueva función para alternar la visibilidad de la contraseña
+  togglePasswordVisibility() {
+    if (this.passwordType === 'password') {
+      this.passwordType = 'text';      // Mostrar la contraseña
+      this.passwordIcon = 'eye';       // Cambiar el icono a "ojo abierto"
+    } else {
+      this.passwordType = 'password';  // Ocultar la contraseña
+      this.passwordIcon = 'eye-off';   // Cambiar el icono a "ojo cerrado"
     }
   }
 
